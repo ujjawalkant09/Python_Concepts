a59751db3ebc506bdeb227e374767a91a0bab946
ujjawalkant <48159084+ujjawalkant09@users.noreply.github.com>
2025-02-04 01:32:00 +0530

    Dependency Injection

--- Design_Pattern/STRADEGY_DESIGN_PATTERN.py
+++ Design_Pattern/STRADEGY_DESIGN_PATTERN.py
@@ -24,30 +24,40 @@ class ResturentsItems(ABC):
 
 
 class GST18PERCENT(ResturentsItems):
+     def __init__(self,res_helper):
+          self.res  = res_helper # Added this to understand dependency injection
      def calculate_tax(self,items:list[Item]):
           total_price = 0
           for item in items:
                total_price += item.price
+          print(self.res.Test) # Added this to understand dependency injection
           return 0.18*total_price
      
 
 class GST12PERCENT(ResturentsItems):
+     def __init__(self,res_helper):
+          self.res  = res_helper # Added this to understand dependency injection
      def calculate_tax(self,items:list[Item]):
           total_price = 0
           for item in items:
                total_price += item.price
+          print(self.res.Test) # Added this to understand dependency injection
           return 0.12*total_price
      
 class GST10PERCENT(ResturentsItems):
+     def __init__(self,res_helper):
+          self.res  = res_helper # Added this to understand dependency injection
      def calculate_tax(self,items:list[Item]):
           total_price = 0
           for item in items:
                total_price += item.price
+          print(self.res.Test)   # Added this to understand dependency injection
           return 0.10*total_price
      
 class ResturentHelper:
     def __init__(self):
           self.items : list[Item] = []
+          self.Test = 99 # Added this to understand dependency injection
     
     def add_items(self,itm:Item):
          self.items.append(itm)
@@ -61,7 +71,11 @@ class ResturentHelper:
 if __name__ == "__main__":
      res_help = ResturentHelper()
      res_help.add_items(Item(Name="Red Bulls",Description="Gives You Wings",price=123))
-     res_help.calculate_tax(GST10PERCENT())
+     
+     gst_10_percent = GST18PERCENT(res_help)
+     res_help.calculate_tax(gst_10_percent)
+     print("---DONE----")
+
 
 
 
